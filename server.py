07ff5527d996f971679f976b0d4f1a9ff30068d8
Markus <markus@medisoftware.de>
2015-10-29 12:55:49 +0100

    debug logging

--- server.py
+++ server.py
@@ -8,17 +8,29 @@ import json
 import feedparser
 import time
 import sys
+import logging
 from rssfeed import RssFeed
 
-mattermost_webhook_url = 'https://192.168.0.238/hooks/cns14rjjfpfrxpgoujxpm7sy3w'  # Paste the Mattermost webhook URL you created here
-mattermost_channel = 'testing'  # Leave this blank to post to the default channel of your webhook
+# Paste the Mattermost webhook URL you created here
+mattermost_webhook_url = 'https://192.168.0.238/hooks/cns14rjjfpfrxpgoujxpm7sy3w'
+# Leave this blank to post to the default channel of your webhook
+mattermost_channel = 'testing'
+# Set the delay between feed pulls to your needs. 5 minutes should be okay.
 delay_between_pulls = 60 * 5
 
 # Your feeds come here:
-# RssFeed('Feed name', 'Feed URL', 'Mattermost username', 'Mattermost channel', show name, show title, show description, show url)
+# RssFeed('Feed name', 'Feed URL', 'Mattermost username', 'Mattermost channel',
+# show name, show title, show description, show url)
+#
 # show name, show title, show description, show url can be True or False; at least one of them should be True
-feeds = {RssFeed('Heise News', 'http://heise.de.feedsportal.com/c/35207/f/653902/index.rss', 'RSS-Bot', 'testing', True, True, True, True),
-         RssFeed('t3n', 'https://feeds2.feedburner.com/aktuell/feeds/rss/', 'RSS-Bot', 'testing', True, True, False, True)
+# show description (longer text or full article) seems not to work with every feed.
+# Set to False it if a feed doesnt´t work.
+feeds = {RssFeed('Heise News', 'http://heise.de.feedsportal.com/c/35207/f/653902/index.rss', 'RSS-Bot', 'testing',
+                 True, True, True, True),
+         RssFeed('t3n', 'https://feeds2.feedburner.com/aktuell/feeds/rss/', 'RSS-Bot', 'testing',
+                 True, True, False, True),
+         RssFeed('Toms', 'http://www.tomshardware.com/articles.xml', 'RSS-Bot', 'testing',
+                 True, True, True, True)
          }
 
 
@@ -37,10 +49,12 @@ def post_text(text, username, channel):
     r = requests.post(mattermost_webhook_url, headers=headers, data=json.dumps(data), verify=False)
 
     if r.status_code is not requests.codes.ok:
-        print('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (mattermost_webhook_url, r.status_code, r.json()))
+        logging.debug('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (mattermost_webhook_url,
+                                                                                               r.status_code, r.json()))
 
 
 if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     if len(mattermost_webhook_url) == 0:
         print('mattermost_webhook_url must be configured. Please see instructions in README.md')
         sys.exit()
@@ -52,10 +66,10 @@ if __name__ == "__main__":
             feed.ArticleUrl = d['entries'][0]['link']
             feed.Description = d['entries'][0]['description']
             if feed.LastTitle != feed.NewTitle:
-                # print(feed.Url)
-                # print('Title: ' + feed.NewTitle + '\n')
-                # print('Link: ' + feed.ArticleUrl + '\n')
-                print(feed.jointext())
+                logging.debug('Feed url: ' + feed.Url)
+                logging.debug('Title: ' + feed.NewTitle + '\n')
+                logging.debug('Link: ' + feed.ArticleUrl + '\n')
+                logging.debug('Posted text: ' + feed.jointext())
                 post_text(feed.jointext(), feed.User, feed.Channel)
                 feed.LastTitle = feed.NewTitle
             else:
