25ab0e6769aeea7dc34bf1d80dfc62bc41fcccc3
Markus <markus@medisoftware.de>
2015-10-30 12:47:35 +0100

    Sample configuration, README Daemonized

--- feedfetcher.py
+++ feedfetcher.py
@@ -10,34 +10,20 @@ import time
 import sys
 import logging
 import settings
-
-mattermost_webhook_url = settings.mattermost_webhook_url
-mattermost_channel = settings.mattermost_channel
-delay_between_pulls = settings.delay_between_pulls
-verify_cert = settings.verify_cert
-feeds = settings.feeds
-
-def post_text(text, username, channel):
-    """
-    Mattermost POST method, posts text to the Mattermost incoming webhook URL
-    """
-    data = {}
-    data['text'] = text
-    if len(username) > 0:
-        data['username'] = username
-    if len(channel) > 0:
-        data['channel'] = channel
-
-    headers = {'Content-Type': 'application/json'}
-    r = requests.post(mattermost_webhook_url, headers=headers, data=json.dumps(data), verify=verify_cert)
-
-    if r.status_code is not requests.codes.ok:
-        logging.debug('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' %
-                      (mattermost_webhook_url, r.status_code, r.json()))
+from daemon import runner
 
 
-if __name__ == "__main__":
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+class App():
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path =  '/tmp/feedfetcher.pid'
+        self.pidfile_timeout = 5
+
+    def run(self):
+        while True:
+            logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     if len(mattermost_webhook_url) == 0:
         print('mattermost_webhook_url must be configured. Please see instructions in README.md')
         sys.exit()
@@ -59,3 +45,33 @@ if __name__ == "__main__":
                 logging.debug('Nothing new. Waiting for good news...')
 
         time.sleep(delay_between_pulls)
+
+mattermost_webhook_url = settings.mattermost_webhook_url
+mattermost_channel = settings.mattermost_channel
+delay_between_pulls = settings.delay_between_pulls
+verify_cert = settings.verify_cert
+feeds = settings.feeds
+
+
+def post_text(text, username, channel):
+    """
+    Mattermost POST method, posts text to the Mattermost incoming webhook URL
+    """
+    data = {}
+    data['text'] = text
+    if len(username) > 0:
+        data['username'] = username
+    if len(channel) > 0:
+        data['channel'] = channel
+
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(mattermost_webhook_url, headers=headers, data=json.dumps(data), verify=verify_cert)
+
+    if r.status_code is not requests.codes.ok:
+        logging.debug('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' %
+                      (mattermost_webhook_url, r.status_code, r.json()))
+
+
+app = App()
+daemon_runner = runner.DaemonRunner(app)
+daemon_runner.do_action()
