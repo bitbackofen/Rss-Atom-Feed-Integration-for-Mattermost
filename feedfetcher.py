285431df3ef0f62441133f8110730a2506b18c37
Markus <markus@medisoftware.de>
2015-10-30 13:05:59 +0100

    Daemonized

--- feedfetcher.py
+++ feedfetcher.py
@@ -13,7 +13,9 @@ import settings
 from daemon import runner
 
 
-class App():
+class App:
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
     def __init__(self):
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
@@ -22,54 +24,47 @@ class App():
         self.pidfile_timeout = 5
 
     def run(self):
-        while True:
-            logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-    if len(mattermost_webhook_url) == 0:
-        print('mattermost_webhook_url must be configured. Please see instructions in README.md')
-        sys.exit()
+        delay_between_pulls = settings.delay_between_pulls
+        feeds = settings.feeds
 
-    while 1:
-        for feed in feeds:
-            d = feedparser.parse(feed.Url)
-            feed.NewTitle = d['entries'][0]['title']
-            feed.ArticleUrl = d['entries'][0]['link']
-            feed.Description = d['entries'][0]['description']
-            if feed.LastTitle != feed.NewTitle:
-                logging.debug('Feed url: ' + feed.Url)
-                logging.debug('Title: ' + feed.NewTitle + '\n')
-                logging.debug('Link: ' + feed.ArticleUrl + '\n')
-                logging.debug('Posted text: ' + feed.jointext())
-                post_text(feed.jointext(), feed.User, feed.Channel)
-                feed.LastTitle = feed.NewTitle
-            else:
-                logging.debug('Nothing new. Waiting for good news...')
+        while 1:
+            for feed in feeds:
+                d = feedparser.parse(feed.Url)
+                feed.NewTitle = d['entries'][0]['title']
+                feed.ArticleUrl = d['entries'][0]['link']
+                feed.Description = d['entries'][0]['description']
+                if feed.LastTitle != feed.NewTitle:
+                    logging.debug('Feed url: ' + feed.Url)
+                    logging.debug('Title: ' + feed.NewTitle + '\n')
+                    logging.debug('Link: ' + feed.ArticleUrl + '\n')
+                    logging.debug('Posted text: ' + feed.jointext())
+                    self.post_text(feed.jointext(), feed.User, feed.Channel)
+                    feed.LastTitle = feed.NewTitle
+                else:
+                    logging.debug('Nothing new. Waiting for good news...')
 
-        time.sleep(delay_between_pulls)
+            time.sleep(delay_between_pulls)
 
-mattermost_webhook_url = settings.mattermost_webhook_url
-mattermost_channel = settings.mattermost_channel
-delay_between_pulls = settings.delay_between_pulls
-verify_cert = settings.verify_cert
-feeds = settings.feeds
+    def post_text(self, text, username, channel):
+        """
+        Mattermost POST method, posts text to the Mattermost incoming webhook URL
+        """
+        verify_cert = settings.verify_cert
+        mattermost_webhook_url = settings.mattermost_webhook_url
 
+        data = {}
+        data['text'] = text
+        if len(username) > 0:
+            data['username'] = username
+        if len(channel) > 0:
+            data['channel'] = channel
 
-def post_text(text, username, channel):
-    """
-    Mattermost POST method, posts text to the Mattermost incoming webhook URL
-    """
-    data = {}
-    data['text'] = text
-    if len(username) > 0:
-        data['username'] = username
-    if len(channel) > 0:
-        data['channel'] = channel
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(mattermost_webhook_url, headers=headers, data=json.dumps(data), verify=verify_cert)
 
-    headers = {'Content-Type': 'application/json'}
-    r = requests.post(mattermost_webhook_url, headers=headers, data=json.dumps(data), verify=verify_cert)
-
-    if r.status_code is not requests.codes.ok:
-        logging.debug('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' %
-                      (mattermost_webhook_url, r.status_code, r.json()))
+        if r.status_code is not requests.codes.ok:
+            logging.debug('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' %
+                          (mattermost_webhook_url, r.status_code, r.json()))
 
 
 app = App()
