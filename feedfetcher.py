6973efa14371d92dc677ece133a5822f9d11e9aa
elpatron68 <elpatron@cepheus.uberspace.de>
2015-11-01 17:53:18 +0100

    Reverted daemonizing

--- feedfetcher.py
+++ feedfetcher.py
@@ -44,18 +44,22 @@ if __name__ == "__main__":
 
     while 1:
         for feed in feeds:
-            d = feedparser.parse(feed.Url)
-            feed.NewTitle = d['entries'][0]['title']
-            feed.ArticleUrl = d['entries'][0]['link']
-            feed.Description = d['entries'][0]['description']
-            if feed.LastTitle != feed.NewTitle:
-                logging.debug('Feed url: ' + feed.Url)
-                logging.debug('Title: ' + feed.NewTitle + '\n')
-                logging.debug('Link: ' + feed.ArticleUrl + '\n')
-                logging.debug('Posted text: ' + feed.jointext())
-                post_text(feed.jointext(), feed.User, feed.Channel)
-                feed.LastTitle = feed.NewTitle
-            else:
-                logging.debug('Nothing new. Waiting for good news...')
+            try:
+                d = feedparser.parse(feed.Url)
+                feed.NewTitle = d['entries'][0]['title']
+                feed.ArticleUrl = d['entries'][0]['link']
+                feed.Description = d['entries'][0]['description']
+                if feed.LastTitle != feed.NewTitle:
+                    logging.debug('Feed url: ' + feed.Url)
+                    logging.debug('Title: ' + feed.NewTitle + '\n')
+                    logging.debug('Link: ' + feed.ArticleUrl + '\n')
+                    logging.debug('Posted text: ' + feed.jointext())
+                    post_text(feed.jointext(), feed.User, feed.Channel)
+                    feed.LastTitle = feed.NewTitle
+                else:
+                    logging.debug('Nothing new. Waiting for good news...')
+            except:
+                logging.critical('Error fetching feed.')
+                continue
 
         time.sleep(delay_between_pulls)
